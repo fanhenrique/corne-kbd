40b33b71fda0a623c8067893902a63ecd6b24487
Henrique Fan <fanhenrique@gmail.com>
2026-05-11 00:05:42 -0300

    accents

--- keymap/keymap.c
+++ keymap/keymap.c
@@ -114,9 +114,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [TEXT] = LAYOUT_split_3x6_3_ex2(
     //----------------------------------------------------------------    ----------------------------------------------------------------
-        KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_GRV,      KC_TILD,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
+        KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,  KC_CIRC,       KC_GRV,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
     //----------------------------------------------------------------    ----------------------------------------------------------------
-        KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,  KC_CIRC,     KC_ACUTE,  KC_H,  KC_J,  KC_K,  KC_L,TD(TD_DQUO_QUOT), KC_DEL,
+        KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,  KC_TILD,     KC_ACUTE,  KC_H,  KC_J,  KC_K,  KC_L,TD(TD_DQUO_QUOT), KC_DEL,
     //----------------------------------------------------------------    ----------------------------------------------------------------
     TD(TD_LSFT_CAPS),LT(FUNCTIONS_NUMPAD,KC_Z), KC_X, KC_C, KC_V, KC_B,         KC_N,  KC_M, KC_COMM, KC_DOT,TD(TD_SLSH_QUES),MO(SPECIAL),
     //----------------------------------------------------------------    ----------------------------------------------------------------
