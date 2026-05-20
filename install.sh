a4b59f28391588878fcf22c8560c2d9354c8e28e
Henrique <fanhenrique@gmail.com>
2026-05-20 17:19:09 -0300

    install

--- install.sh
+++ install.sh
@@ -1,7 +1,36 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
 set -e
 
+printf "\n==> Updating system...\n"
 sudo apt update
-sudo apt install -y udisks2
 
-sudo systemctl enable --now udisks2
+printf "\n==> Installing dependencies...\n"
+sudo apt install -y \
+    udisks2
+
+# gcc-arm-none-eabi
+
+printf "\n==> Installing QMK...\n"
+curl -fsSL https://install.qmk.fm | sh
+
+# ensure PATH
+if ! grep -q 'export PATH="$HOME/.local/bin:$PATH"' ~/.bashrc; then
+    echo 'PATH="$HOME/.local/bin:$PATH"' >> $HOME/.bashrc && source $HOME/.bashrc
+fi
+
+printf "\n==> QMK Setup...\n"
+
+cd "$HOME"
+
+if [ ! -d "$HOME/qmk_firmware" ]; then
+    qmk setup -y
+else
+    printf "QMK Firmware already exists, skipping clone.\n"
+fi
+
+printf "\n==> Testing installation...\n"
+qmk doctor
+
+printf "\n==> Installation completed!\n"
+printf "IMPORTANT: Restart your session (logout/login) before using QMK.\n"
