43b22e5ae2afd28e1f42332ff25c9c0a7a28a431
Henrique <fanhenrique@gmail.com>
2026-09-08 19:49:25 -0300

    keymap

--- keymap/keymap.c
+++ keymap/keymap.c
@@ -91,22 +91,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [FUNCTIONS_NUMPAD] = LAYOUT_split_3x6_3_ex2(
     //----------------------------------------------------------------    ----------------------------------------------------------------
-          KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,KC_F6,TD(TDL_FUNCTIONS_NUMPAD),   TD(TDL_BASE), KC_PLUS,   KC_7,     KC_8,   KC_9,  KC_ASTR, KC_BSPC,
+          KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6, XXXXXXX,   TD(TDL_BASE), KC_PLUS,  KC_7,   KC_8,   KC_9,  KC_ASTR, KC_BSPC,
     //----------------------------------------------------------------    ----------------------------------------------------------------
-          KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, XXXXXXX,      XXXXXXX, KC_MINS,   KC_4,     KC_5,   KC_6,  KC_SLSH,  KC_DEL,
+          KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, XXXXXXX,      XXXXXXX,   KC_MINS,  KC_4,   KC_5,   KC_6,  KC_SLSH,  KC_DEL,
     //----------------------------------------------------------------    ----------------------------------------------------------------
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,              KC_COMM,   KC_1,     KC_2,   KC_3,   KC_EQL, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                          KC_COMM,  KC_1,   KC_2,   KC_3,   KC_EQL, XXXXXXX,
     //----------------------------------------------------------------    ----------------------------------------------------------------
                                             XXXXXXX, XXXXXXX, KC_SPC,       KC_ENT, KC_DOT,  KC_0
     ),
 
      [MOUSE] = LAYOUT_split_3x6_3_ex2(
     //----------------------------------------------------------------    ----------------------------------------------------------------
-         KC_ESC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    TD(TDL_BASE), MS_BTN3, MS_BTN1, MS_UP, MS_BTN2, XXXXXXX, XXXXXXX,
+         KC_ESC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    TD(TDL_BASE), MS_BTN3, MS_BTN1, MS_UP, MS_BTN2, XXXXXXX,XXXXXXX,
     //----------------------------------------------------------------    ----------------------------------------------------------------
-         KC_TAB, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, MS_WHLU, MS_LEFT, MS_DOWN,MS_RGHT, XXXXXXX, XXXXXXX,
+         KC_TAB, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, MS_WHLU, MS_LEFT, MS_DOWN, MS_RGHT, XXXXXXX, XXXXXXX,
     //----------------------------------------------------------------    ----------------------------------------------------------------
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                 MS_WHLD, XXXXXXX, XXXXXXX, TD(TDL_MOUSE), XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        MS_WHLD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     //----------------------------------------------------------------    ----------------------------------------------------------------
                                         XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX
     ),
